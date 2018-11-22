94b885428f4275073dfae1ad9575ebfd750584ed
Bruno Bandeira <burn@gamersclub.com.br>
2018-11-22 15:18:07 -0200

    remoção de comentários

--- src/config/database.js
+++ src/config/database.js
@@ -1,9 +1,4 @@
 const knexfile = require('../knexfile')
 const knex = require('knex')(knexfile)
+
 module.exports = knex
-
-// knex('beers').insert({
-//     name: 'Xulapa'
-// }).then(data => console.log(data))
-
-// knex('beers').then(result => console.log(result))
